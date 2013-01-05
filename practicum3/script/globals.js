4dfd8544ed5da547311a34bbb68c5716d5daa993
tamis-laan <tamis.vanderlaan@gmail.com>
2013-01-05 12:52:38 +0100

    Added data file as test also in globals

--- practicum3/script/globals.js
+++ practicum3/script/globals.js
@@ -1,9 +1,23 @@
-var year = 1960;
+var year = 2008;
+var life_expectancy_data = [];
+
 function updateSlider(value)
 {
 	var yearDisp = $('#yearDisp');
 	year = value;
-	console.log(year);
+	// console.log(year);
 	yearDisp.html(""+ year);
+
+	//Load in the life expectancy data
+	d3.csv("/data/datasets/life_expectancy_test.csv", function(csv)
+	{
+		csv.forEach(function(row)
+		{
+			console.log(row);	
+			life_expectancy_data[row["Country Name"]] = row["2008"];
+		});
+	});
+
+	console.log(life_expectancy_data);
 }
-updateSlider(1960);
+
