c4a418028f5f508ecc34f303a73b40a1b17e5c66
Laurent Verweijen <lverweijen>
2013-01-07 00:35:01 +0100

    y as toegevoegd
    pogingen om x as aan te passen gerevert

--- practicum3/script/barchart.js
+++ practicum3/script/barchart.js
@@ -102,6 +102,11 @@ function Barchart(id, indicator, data) {
             .tickPadding(6)
             .orient("bottom");
 
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickFormat(d3.format(".2s"));
+
         svg.attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
@@ -130,6 +135,16 @@ function Barchart(id, indicator, data) {
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(indicator);
     }
 
     //updateCountries([]);
